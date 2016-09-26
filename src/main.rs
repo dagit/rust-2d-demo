e66a33a0fae43a7bb525c8b918cb0e633cb739e1
Jason Dagit <dagitj@gmail.com>
2016-09-26 16:06:31 -0700

    Improve font rendering move assets around

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ extern crate image;
 use rusttype::Font;
 use glium::texture::texture2d::Texture2d;
 
-use std::borrow::Cow;
 use std::marker::PhantomData;
 use std::io::Cursor;
 
@@ -21,9 +20,23 @@ pub struct DemoDrawer<'a> {
     frame:      glium::Frame,
 }
 
+pub struct TextInfo<'a> {
+    font: Font<'a>,
+    size: f32,
+}
+
+
+pub struct UiState<'a> {
+    #[allow(dead_code)]
+    immi_state: immi::UiState,
+    background: Texture2d,
+    textinfo:   TextInfo<'a>,
+}
+
+
 impl<'a> immi::Draw for DemoDrawer<'a> {
     type ImageResource = Texture2d;
-    type TextStyle     = Font<'a>;
+    type TextStyle     = TextInfo<'a>;
 
     fn draw_triangle(&mut self,
                      texture:   &Texture2d,
@@ -48,7 +61,7 @@ impl<'a> immi::Draw for DemoDrawer<'a> {
         let indices       = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
         let uniforms      = uniform! {
             matrix: Into::<[[f32; 4]; 4]>::into(*matrix),
-            tex: texture,
+            tex:    texture,
         };
 
         self.frame.draw(&vertex_buffer, &indices, &self.ui_shader,
@@ -69,34 +82,52 @@ impl<'a> immi::Draw for DemoDrawer<'a> {
     ///
     /// This function should not try to preseve the aspect ratio of the
     /// image. This is handled by the caller.
-    fn draw_glyph(&mut self, font: &Font<'a>, c: char, matrix: &immi::Matrix)
+    fn draw_glyph(&mut self, textinfo: &TextInfo<'a>, c: char, matrix: &immi::Matrix)
     {
-        // TODO: this information should be font value instead of
-        // directly passing the font.
-        let scale     = rusttype::Scale::uniform(24.0);
+        let font      = &textinfo.font;
+        let scale     = rusttype::Scale::uniform(textinfo.size);
         let v_metrics = font.v_metrics(scale);
         let offset    = rusttype::point(0.0, v_metrics.ascent);
 
         let glyphs: Vec<rusttype::PositionedGlyph> = font
-            .layout(&c.to_string(), rusttype::Scale::uniform(24.0), offset)
+            .layout(&c.to_string(), scale, offset)
             .collect();
 
+        fn index(x: u32, y: u32, width: u32, depth: u32) -> usize {
+            (y * width * depth + x * depth) as usize
+        }
+        
         for glyph in glyphs {
             if let Some(bb) = glyph.pixel_bounding_box() {
                 let (width, height) = (bb.width() as u32, bb.height() as u32);
-                let mut pixels = vec![0; (height * width) as usize];
+                let depth = 4;
+                // This is wasteful and we can do better. We really
+                // only need to set one channel and then use a special
+                // shader that knows to look at that one channel and
+                // treat it as a greyscale value.
+                let size = height as usize * width as usize * depth as usize;
+                let mut pixels = vec![0; size];
                 glyph.draw(|x,y,v| {
                     let v = ((v * 255.0) + 0.5).floor().max(0.0).min(255.0) as u8;
-                    pixels[(y * width + x) as usize] = v;
+                    for i in 0..(depth-1) {
+                        pixels[index(x,y,width,depth) + i as usize] = v;
+                    }
+                    pixels[index(x,y,width,depth) + depth as usize - 1] = 255;
                 });
-                let raw = glium::texture::RawImage2d {
-                    data:   Cow::Borrowed(pixels.as_slice()),
-                    width:  width,
-                    height: height,
-                    format: glium::texture::ClientFormat::U8
-                };
+                let raw = glium::texture::RawImage2d::from_raw_rgba(pixels, (width, height));
                 let tex = glium::texture::texture2d::Texture2d::new(self.display, raw).unwrap();
-                self.draw_image(&tex, matrix);
+
+                // I'm not sure why, but we need to invert the y-axis
+                // here.  From the docs I would have thought we didn't
+                // need to do that.
+                let inverty = immi::Matrix([
+                    [1.0,  0.0],
+                    [0.0, -1.0],
+                    [0.0,  0.0],
+                    ]
+                );
+                let invertedmat = inverty * *matrix;
+                self.draw_image(&tex, &invertedmat);
             }
         }
     }
@@ -105,27 +136,53 @@ impl<'a> immi::Draw for DemoDrawer<'a> {
     ///
     /// This value is usually somewhere around `1.2`.
     /// TODO: get this from the font info
-    fn line_height(&self, _: &Font<'a>) -> f32 { 1.2 }
-    
-    fn kerning(&self, font: &Font<'a>, a: char, b: char) -> f32
+    fn line_height(&self, _: &TextInfo<'a>) -> f32 { 1.2 }
+
+    #[allow(unused_variables)]
+    fn kerning(&self, textinfo: &TextInfo<'a>, a: char, b: char) -> f32
     {
-        let scale = rusttype::Scale::uniform(24.0);
-        font.pair_kerning(scale, a, b)
+        // For some reason all my attempts to calculate the kerning have been wrong
+        // so I just return 0.0 here. Which is also not great, but works.
+        // let scale = rusttype::Scale::uniform(textinfo.size);
+        // textinfo.font.pair_kerning(scale, a, b);
+        0.0
+        
     }
     
-    fn glyph_infos(&self, font: &Font<'a>, c: char) -> immi::GlyphInfos {
-        let scale     = rusttype::Scale::uniform(24.0);
-        let glyph     = font.glyph(c).unwrap().scaled(scale);
-        let h_metrics = glyph.h_metrics();
+    fn glyph_infos(&self, textinfo: &TextInfo<'a>, c: char) -> immi::GlyphInfos {
+        let font      = &textinfo.font;
+        let scale     = rusttype::Scale::uniform(textinfo.size);
         let v_metrics = font.v_metrics(scale);
-        //let factor = font.info.scale_for_pixel_height(scale.y) * (scale.x / scale.y);
-        // TODO: this might be totally wrong. Width/height should probably be the
-        // bounding box width/height.
-        immi::GlyphInfos { width: h_metrics.advance_width,
-                          height: v_metrics.ascent - v_metrics.descent,
-                          x_offset: 0.0,
-                          y_offset: 1.0,
-                          x_advance: 1.0 }
+        let offset    = rusttype::point(0.0, v_metrics.ascent);
+        let glyphs: Vec<rusttype::PositionedGlyph> =
+            font.layout(&c.to_string(), scale, offset)
+            .collect();
+        let ems: Vec<rusttype::PositionedGlyph> =
+            font.layout(&'M'.to_string(), scale, offset)
+            .collect();
+        let glyph   = &glyphs[0];
+        let em      = &ems[0];
+        if let Some(glyphbb) = glyph.pixel_bounding_box() {
+            if let Some(embb) = em.pixel_bounding_box() {
+                // This doesn't seem to be quite right, but it's close
+                // in some cases.
+                return immi::GlyphInfos { width: glyphbb.width() as f32 / embb.width() as f32,
+                                         height: glyphbb.height() as f32 / embb.height() as f32,
+                                         x_offset: 0.0,
+                                         y_offset: 1.0,
+                                         x_advance: 1.0}
+            } else {
+                //println!("No embb");
+            }
+        } else {
+            //println!("no glyphbb for '{}'", c);
+        }
+
+        // This is the default value if we couldn't calculate something
+        // more reasonable
+        immi::GlyphInfos { width: 1.0, height: 1.0, x_offset: 0.0,
+                          y_offset: 1.0, x_advance: 1.0}
+
     }
     
 }
@@ -133,8 +190,11 @@ impl<'a> immi::Draw for DemoDrawer<'a> {
 fn main() {
     use glium::{DisplayBuild, Surface};
 
+    let window_width  = 1024.0;
+    let window_height = 768.0;
     let display      = glium::glutin::WindowBuilder::new()
         .with_vsync()
+        .with_dimensions(window_width as u32, window_height as u32)
         .with_srgb(Some(true))
         .build_glium()
         .unwrap();
@@ -169,38 +229,39 @@ fn main() {
             "#
         }).unwrap();
 
-    let font_data    = include_bytes!("../assets/font.ttf");
+    //let font_data    = include_bytes!("../assets/font.ttf");
+    let font_data    = include_bytes!("../assets/Arial Unicode.ttf");
     let collection   = rusttype::FontCollection::from_bytes(font_data as &[u8]);
     let font         = collection.into_font().unwrap();
 
-    struct UiState<'a> {
-        #[allow(dead_code)]
-        immi_state: immi::UiState,
-        
-        #[allow(dead_code)]
-        background: Texture2d,
-        
-        #[allow(dead_code)]
-        font:       Font<'a>,
-    }
-
-    let image   = image::load(Cursor::new(&include_bytes!("../bee-trixel.png")[..]),
+    let image   = image::load(Cursor::new(&include_bytes!("../assets/bee-trixel.png")[..]),
                              image::PNG).unwrap().to_rgba();
     let id      = image.dimensions();
     let image   = glium::texture::RawImage2d::from_raw_rgba_reversed(image.into_raw(), id);
     let texture = glium::texture::Texture2d::new(&display, image).unwrap();
 
+    // let dpi_factor = display.get_window().unwrap().hidpi_factor();
+    // println!("dpi_factor is {}", dpi_factor);
+    
     let mut ui_state : UiState = UiState {
         immi_state: Default::default(),
         background: texture,
-        font:       font,
+        textinfo:   TextInfo {
+            font: font,
+            size: 1000.0,
+        }
     };
 
     fn draw_ui<'a>(ctxt: &immi::DrawContext<DemoDrawer<'a>>, ui_state: &mut UiState<'a>)
     {
-        // This doesn't render correctly, not sure why
+
         immi::widgets::image::draw(ctxt, &ui_state.background, &immi::Alignment::top());
-        //immi::widgets::label::flow(ctxt, &ui_state.font, &"Hello, World!", &immi::HorizontalAlignment::Left);
+        // This doesn't render correctly, not sure why
+        // immi::widgets::label::flow(ctxt, &ui_state.textinfo, &"Hello, World!",
+        //                         &immi::HorizontalAlignment::Left);
+        immi::widgets::label::contain(ctxt, &ui_state.textinfo, &"Hello, World!",
+                                   &immi::Alignment::center());
+
     }
 
     'main: loop {
@@ -221,7 +282,7 @@ fn main() {
         };
         drawer.frame.clear_color(0.0, 0.0, 1.0, 1.0);
         let ui_context = immi::draw();
-        let ui_context = ui_context.draw(1024.0, 768.0, &mut drawer, None, false, false);
+        let ui_context = ui_context.draw(window_width, window_height, &mut drawer, None, false, false);
         
         draw_ui(&ui_context, &mut ui_state);
         ui_context.draw().frame.set_finish().unwrap();
